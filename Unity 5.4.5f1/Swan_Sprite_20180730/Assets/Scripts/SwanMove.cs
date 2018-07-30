f600fbe25139f10ca5927a90179b8cab311ae9b6
RoronoaZoroZrh <137147783@qq.com>
2018-07-30 10:46:51 +0800

    Swan_Sprite_20180730

---
+++ Unity 5.4.5f1/Swan_Sprite_20180730/Assets/Scripts/SwanMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SwanMove : MonoBehaviour
+{
+    private float m_fSpeed = 4.0f;
+
+    private void Start()
+    {
+        transform.position = new Vector3(22, 3, 0);
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > -22)
+        {
+            transform.Translate(Vector3.right * -m_fSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector3(22, 3, 0);
+        }
+    }
+}
