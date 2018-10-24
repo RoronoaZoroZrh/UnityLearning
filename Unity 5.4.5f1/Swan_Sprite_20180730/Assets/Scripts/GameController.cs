48a0f6f100674eb09de6362fcd3e9b4a21461a4c
RoronoaZoroZrh <137147783@qq.com>
2018-10-24 10:01:21 +0800

    Swan_Sprite_20180730

---
+++ Unity 5.4.5f1/Swan_Sprite_20180730/Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    #region Properties
+    //保龄球
+    public GameObject BowlingBall = null;
+    private GameObject m_bowlingBallNew = null;
+
+    //宽度
+    private float m_maxWidth = float.MinValue;
+
+    //时间
+    private float m_fallTime = 2f;
+    #endregion
+
+    #region Methods
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 screenPos = new Vector3(Screen.width, 0, 0);
+        Vector3 moveWidth = Camera.main.ScreenToWorldPoint(screenPos);
+        float ballWidth = BowlingBall.GetComponent<Renderer>().bounds.extents.x;
+        m_maxWidth = moveWidth.x - ballWidth;
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_fallTime = m_fallTime - Time.deltaTime;
+        if (m_fallTime < 0)
+        {
+            m_fallTime = Random.Range(1.5f, 2.0f);
+            float posX = Random.Range(-m_maxWidth, m_maxWidth);
+            Vector3 spawnPosition = new Vector3(posX, transform.position.y, 0);
+            m_bowlingBallNew = (GameObject)Instantiate(BowlingBall, spawnPosition, Quaternion.identity);
+            Destroy(m_bowlingBallNew, 10);
+        }
+    }
+}
---
+++ Unity 5.4.5f1/Swan_Sprite_20180730/Assets/Scripts/HatController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatController : MonoBehaviour
+{
+
+    #region Properties
+    //帽子位置
+    private Vector3 m_hatPos;
+
+    //宽度
+    private float m_maxWidth;
+
+    //特效
+    public GameObject Effect;
+    #endregion
+
+    #region Methods
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 sceenPos = new Vector3(Screen.width, 0, 0);
+        Vector3 moveWidth = Camera.main.ScreenToWorldPoint(sceenPos);
+        float hatWidth = GetComponent<Renderer>().bounds.extents.x;
+        m_hatPos = transform.position;
+        m_maxWidth = moveWidth.x - hatWidth;
+    }
+
+    // Update is called once per physics timestep
+    private void FixedUpdate()
+    {
+        Vector3 rawPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 hatPos = new Vector3(rawPos.x, rawPos.y, 0);
+        hatPos.x = Mathf.Clamp(hatPos.x, -m_maxWidth, m_maxWidth);
+        GetComponent<Rigidbody2D>().MovePosition(hatPos);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject newEffect = (GameObject)Instantiate(Effect, transform.position, transform.rotation);
+        newEffect.transform.parent = transform;
+        Destroy(newEffect, 1f);
+        Destroy(collision.gameObject);
+    }
+    #endregion
+}
