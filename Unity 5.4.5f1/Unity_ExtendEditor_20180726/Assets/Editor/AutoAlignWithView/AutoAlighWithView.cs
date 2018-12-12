c6e1022c1b782c3956a0af6308ce00aa6fa53685
RoronoaZoroZrh <137147783@qq.com>
2018-12-12 10:29:04 +0800

    UnityStudy_20181210

---
+++ Unity 5.4.5f1/Unity_ExtendEditor_20180726/Assets/Editor/AutoAlignWithView/AutoAlighWithView.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ *  @author zhangronghuan
+ *  @date   2018-11-26 11:43:18
+ *  @brief  Game视图镜头自动与Scene视图镜头同步
+ */
+using UnityEditor;
+using UnityEngine;
+
+internal class AutoAlighWithView
+{
+    [MenuItem("ZRHPersonal/AutoAlighWithView", false, 0)]
+    private static void Excute()
+    {
+        EditorApplication.update += () =>
+        {
+            Camera.main.transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+            Camera.main.transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
+        };
+    }
+}
