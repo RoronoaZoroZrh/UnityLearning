000d305689e555466670484bad0df69d173063b2
RoronoaZoroZrh <137147783@qq.com>
2018-12-12 16:36:48 +0800

    CameraComponent

---
+++ Unity 5.4.5f1/UnityStudy_20181210/Assets/UNet/Scripts/Component/CameraComponent.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace UNetStuty
+{
+    //Camera Control Component
+    public class CameraComponent : MonoBehaviour
+    {
+        //Rotation delta
+        private float _deltaX = 0f;
+
+        //Rotation delta
+        private float _deltaY = 0f;
+
+        //Mouse Position
+        private Vector3 _mousePosition;
+
+        void Update()
+        {
+            //Scale
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                gameObject.transform.localPosition = gameObject.transform.localPosition + Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * 10;
+            }
+
+            //Rotate
+            if (Input.GetMouseButton(1))
+            {
+                _deltaX = _deltaX + Input.GetAxis("Mouse X") * 5f;
+                _deltaY = _deltaY + Input.GetAxis("Mouse Y") * 5f;
+                _deltaX = MathUtilities.ClampAngle(_deltaX, -360, 360);
+                _deltaY = MathUtilities.ClampAngle(_deltaY, -70, 70);
+                gameObject.transform.rotation = Quaternion.Euler(-_deltaY, -_deltaX, 0);
+            }
+
+            //Move
+            if (Input.GetMouseButtonDown(2))
+            {
+                _mousePosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(2))
+            {
+                Vector3 offset = Input.mousePosition - _mousePosition;
+                offset = gameObject.transform.rotation * offset;
+                gameObject.transform.localPosition = gameObject.transform.localPosition + offset * -0.005f;
+                _mousePosition = Input.mousePosition;
+            }
+        }
+    }
+}
---
+++ Unity 5.4.5f1/UnityStudy_20181210/Assets/Utilities/MathUtilities.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UNetStuty
+{
+    public class MathUtilities
+    {
+        public static float ClampAngle(float curAngle, float minAngle, float maxAngle)
+        {
+            while (curAngle <= -360) curAngle = curAngle + 360;
+            while (curAngle >= +360) curAngle = curAngle - 360;
+            return Mathf.Clamp(curAngle, minAngle, maxAngle);
+        }
+    }
+}
